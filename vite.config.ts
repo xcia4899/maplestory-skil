dfba259ba069c186a99928033de3dbd0582dbffe
xcia4899 <xcia48996531@gmail.com>
2025-10-15 18:27:44 +0800

    build: pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/maplestory-skil/', // ← 關鍵：加上你的 repo 名稱
   plugins: [
     vue(),
     vueDevTools(),
